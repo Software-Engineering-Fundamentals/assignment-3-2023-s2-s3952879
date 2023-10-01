e4650811412a6edd7a22eca45fdfb2c66252dd95
Cameron Lindsay <s3952879@student.rmit.edu.au>
2023-10-01 22:37:28 +1100

    adding comments

--- Activity2/src/main/java/model/manager.java
+++ Activity2/src/main/java/model/manager.java
@@ -2,13 +2,14 @@ import java.sql.Date;
 
 public class manager extends user{
     
-    private String managerID;
-    protected String login;
-    protected String password;
+    private String managerID; //unique ID to identify this manager
+    protected String login; //this manager's username
+    protected String password; //this manager's password
 
+    // constructor
     public manager(String name, String address, Date dob, String email, String contactNumber, String emergencyContact,
                         String managerID, String login, String password) {
-
+        // as manager is built on user, also perform the user constructor
         super(name, address, dob, email, contactNumber, emergencyContact);
         this.managerID= managerID;
         this.login = login;
@@ -16,11 +17,14 @@ public class manager extends user{
 
     }
     
+    // provide response then post the answer
     public void manageEnquiry(enquiry query) {
-        query.setResponse("that question was easily answered if you had simply studied the week 107 content for the course that test pertains to");
+        query.setResponse("that question was easily answered if you had simply studied the week 107 " +
+                "content for the course that test pertains to");
         query.answer();
     }
 
+    // close given enquiry
     public void closeEnquiry(enquiry query) {
         query.close();
     }
