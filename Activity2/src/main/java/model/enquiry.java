b5461db9be7da6389a911fa0207ebfad96822810
Cameron Lindsay <s3952879@student.rmit.edu.au>
2023-10-01 22:07:29 +1100

    renamed status to open to reflect function

--- Activity2/src/main/java/model/enquiry.java
+++ Activity2/src/main/java/model/enquiry.java
@@ -2,14 +2,14 @@ public class enquiry {
     private String enquiryID;
     private String contents; // question asked by user
     private String response;
-    private boolean status;
+    private boolean open;
     private String userID; // linked to x user
 
     // constructor
-    public enquiry(String enquiryID, String contents, boolean status, String userID, String response) {
+    public enquiry(String enquiryID, String contents, boolean open, String userID, String response) {
         this.enquiryID = enquiryID;
         this.contents = contents;
-        this.status = status;
+        this.open = open;
         this.userID = userID;
         this.response = response;
     }
