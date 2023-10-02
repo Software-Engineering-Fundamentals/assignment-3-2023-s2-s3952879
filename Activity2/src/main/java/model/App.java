e5aa4b72f65a5a7997be459748841a645e3b8973
Alexandros Zafirelis <s3530393@student.rmit.edu.au>
2023-10-02 20:26:45 +1100

    generate password/login

--- Activity2/src/main/java/model/App.java
+++ Activity2/src/main/java/model/App.java
@@ -21,20 +21,27 @@ public class App {
 
 
 
-        // Add comments to the code to explain the mapping from class and sequence diagrams to code. A code without comments will be penalized
-        // user selects Registers course 
+        // User selects Registers course 
 
         // user is prompted to enter their details starting with course details
         course testCourse = new course("SOFT1","Software Fundamentals", "School of Computing", "This is a course about software fundamentals", 500, "3 Months", false);
         // user provides correct details
-        // asumes there is error checking for each of the details  
+        // assumes there is error checking for each of the details  
 
         // user prompted for personal details and instructor ID
         instructor newInstructor = new instructor("AZ33","Alex", "Melbourne", "01/01/2000", "test@gmail.com", "0404 000 000", "0404 000 000");
 
+        // System requests payment information by user
+
+    
         // system is generating a login and password for the instructor
         String login = newInstructor.generateLogin();
         String password = newInstructor.generatePass();
+
+        newInstructor.setLogin(newInstructor.generateLogin());
+        System.out.println(newInstructor.getLogin());
+        newInstructor.setPass(newInstructor.generatePass());
+        System.out.println(newInstructor.getPassword());
 
         // system is setting the password to the unique instructor object
         newInstructor.setLogin(login);
@@ -42,7 +49,8 @@ public class App {
   
         // system prompts for payment
         System.out.println(newInstructor.getInstructorID());
-        
+        System.out.println("Work");
+
         
 
 
--- Activity2/src/main/java/model/instructor.java
+++ Activity2/src/main/java/model/instructor.java
@@ -1,8 +1,10 @@
-import java.sql.Date;
+import java.nio.charset.Charset;
+import java.util.Random;
 
 public class instructor extends user {
     
     private String instructorID;
+    protected String paymentInfo;
     protected String login;
     protected String password;
 
@@ -16,7 +18,13 @@ public class instructor extends user {
     public String getInstructorID() {
         return this.instructorID;
     }
-
+    public String getLogin() {
+        return this.login;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    
     // sets login
     public void setLogin(String login) {
         this.login = login;
@@ -26,19 +34,42 @@ public class instructor extends user {
     public void setPass(String password) {
         this.password = password;
     }
+        
+    public void setPayment(String payment) {
+        this.paymentInfo = payment;
+    }
 
     // generate random ID for instructor
     public String generateLogin() {
-        String login = "test";
-        
-        return login;
+
+
+      // alphanumber 10 digit string builder  
+      char[] array ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+      StringBuilder random = new StringBuilder();
+
+      for(int i =0; i < 10; i++) {
+          int index = (int) (Math.random()*array.length);
+          random.append(array[index]);
+      }
+      String login = random.toString();
+
+      return login;
 
     }
+
     // generate random Password for instructor
     public String generatePass() {
-        String password = "test";
+      // alphanumber 10 digit string builder  
+      char[] array ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+      StringBuilder random = new StringBuilder();
 
-        return password;
+      for(int i =0; i < 10; i++) {
+          int index = (int) (Math.random()*array.length);
+          random.append(array[index]);
+      }
+      String password = random.toString();
+
+      return password;
 
     }
 }
